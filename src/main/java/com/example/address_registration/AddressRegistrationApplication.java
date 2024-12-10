1218d74ae30648662fa8fa5414643b0dea2d5799
Sona Ebrazi <behnam.safari@2mnordic.com>
2024-12-10 12:03:16 +0100

    Initial commit with Spring Boot project

---
+++ src/main/java/com/example/address_registration/AddressRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.address_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/address_registration/AddressRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.address_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
